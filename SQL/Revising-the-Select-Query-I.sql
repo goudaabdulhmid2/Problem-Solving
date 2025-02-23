b93beaa27eb76762454203cf7bd9641c6ebbc171
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-02-23 23:28:04 +0200

    Revising the Select Query I

---
+++ SQL/Revising-the-Select-Query-I.sql
@@ -0,0 +1,3 @@
+// ============================ problem link: https://leetcode.com/problems/swap-salary/ ============================
+SELECT * 
+FROM CITY WHERE CountryCode = 'USA' AND POPULATION > 100000
