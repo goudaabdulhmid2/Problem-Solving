3b4be44c15f81ca80b31622ba8aca1d9ca8745ef
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-03-02 15:16:42 +0200

    Two Sum

---
+++ neetcode/Arrays_Hashing/src/Two_Sum.ts
@@ -0,0 +1,17 @@
+class TwoSum {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum(nums: number[], target: number): number[] | undefined {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+    return undefined;
+  }
+}
